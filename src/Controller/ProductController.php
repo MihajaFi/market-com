3343ec76ed152321a35cd93301a377a61737bc7f
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2026-02-13 16:12:46 +0100

    fix: Change importation

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,7 +8,7 @@ use App\Service\ServiceImpl\ProductServiceImpl;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/products')]
