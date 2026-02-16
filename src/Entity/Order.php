5c5235d36f11ec9437697838ce47768bd1c51b3c
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2026-02-16 16:33:26 +0100

    feat: Create Order entity

---
+++ src/Entity/Order.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: 'orders')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'date')]
+    private ?\DateTimeInterface $orderDate = null;
+
+    #[ORM\Column(type: 'float')]
+    private ?float $totalAmount = 0;
+
+    #[ORM\Column(length: 25)]
+    private ?string $status = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    #[ORM\JoinColumn(name: 'id_user', nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\OneToMany(
+        mappedBy: 'order',
+        targetEntity: OrderItem::class,
+        cascade: ['persist', 'remove'],
+        orphanRemoval: true
+    )]
+    private Collection $items;
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+        $this->orderDate = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOrderDate(): ?\DateTimeInterface
+    {
+        return $this->orderDate;
+    }
+
+    public function setOrderDate(\DateTimeInterface $date): static
+    {
+        $this->orderDate = $date;
+        return $this;
+    }
+
+    public function getTotalAmount(): ?float
+    {
+        return $this->totalAmount;
+    }
+
+    public function setTotalAmount(float $amount): static
+    {
+        $this->totalAmount = $amount;
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): static
+    {
+        $this->user = $user;
+        return $this;
+    }
+    
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(OrderItem $item): static
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->setOrder($this);
+        }
+        return $this;
+    }
+
+    public function removeItem(OrderItem $item): static
+    {
+        $this->items->removeElement($item);
+        return $this;
+    }
+}
